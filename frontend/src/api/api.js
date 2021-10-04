a1dd48ae4e9782473889fe508fa00e6d0f08de1a
cywk <yongwankang@hotmail.com>
2021-10-04 20:46:06 +0800

    fetching question information

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,17 +1,22 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3030/api',
-})
-
+  baseURL: "http://localhost:3030/api",
+});
+const api2 = axios.create({
+  baseURL: "http://localhost:3030/question",
+});
 
 // Account registration and login
-export const registerAccount = payload => api.post(`/register`, payload)
-export const loginAccount = payload => api.post(`/login`, payload)
+export const registerAccount = (payload) => api.post(`/register`, payload);
+export const loginAccount = (payload) => api.post(`/login`, payload);
 
+// Question
+export const fetchQuestion = (endpoint) => api2.get(endpoint);
 const apis = {
-    registerAccount,
-    loginAccount,
-}
+  registerAccount,
+  loginAccount,
+  fetchQuestion,
+};
 
-export default apis
+export default apis;
--- frontend/src/components/Editor/question.js
+++ frontend/src/components/Editor/question.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
+import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
+import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import apis from "../../api/api";
 
 const useStyles = makeStyles({
   root: {
-    width: "100%",
+    minwidth: "100%",
     height: "100%",
   },
   bullet: {
@@ -17,8 +18,19 @@ const useStyles = makeStyles({
     transform: "scale(0.8)",
   },
   title: {
-    fontSize: 35,
-    marginBottom: 20,
+    fontSize: 25,
+    marginBottom: 10,
+  },
+  difficulty: {
+    fontSize: 15,
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    width: 30,
+    height: 20,
+    padding: "0 30px",
   },
   pos: {
     marginBottom: 12,
@@ -26,19 +38,35 @@ const useStyles = makeStyles({
 });
 
 export default function Question() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  useEffect(() => {
+    apis
+      .fetchQuestion("/6158535f5e5ae6f89f89c430/")
+      .then((res) => {
+        console.log(res.data);
+        // var question = JSON.parse(res.data.question);
+        // console.log(res.data);
+        setTitle(res.data.question.questionStatement);
+        setDifficulty(res.data.question.difficulty);
+      })
+      .catch((err) => {
+        console.log("something wrong");
+        console.log(err);
+      });
+  }, []);
+
   const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardContent>
+        <Box className={classes.difficulty}>{difficulty}</Box>
         <Typography className={classes.title} variant="h1" color="textPrimary">
-          Word of the Day
+          {title ? title : "Question not found"}
         </Typography>
         <Typography variant="body2" component="p">
-          This text is styled with some of the text formatting properties. The
-          heading uses the text-align, text-transform, and color properties. The
-          paragraph is indented, aligned, and the space between characters is
-          specified. The underline is removed from this colored "Try it
-          Yourself" link.
+          {body}
           <br />
         </Typography>
       </CardContent>
