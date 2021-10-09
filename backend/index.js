04e43886b97bd75c6f5b59bcdd037bb966a093a5
Oong Jie Xiang <oongjiexiang@yahoo.com>
2021-10-09 16:42:42 +0800

    set up test cases

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const config = require('config')
 
 const db = require('./db')
 const AuthRouter = require('./routers/auth')
 const QuestionRouter = require('./routers/questionRoutes');
 
 const app = express()
-const apiPort = 3030
+const apiPort = process.env.PORT || 3030
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
-app.use(morgan('dev'))
+if(config.util.getEnv('NODE_ENV') !== 'test'){
+    app.use(morgan('dev'))
+}
 app.use(bodyParser.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
@@ -24,3 +27,5 @@ app.use('/api', AuthRouter)
 app.use('/api/question', QuestionRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+module.exports = app;
