65d228d1e0e18b58c46dfdbc0b6da41a80fc8b29
Oong Jie Xiang <oongjiexiang@yahoo.com>
2021-10-18 10:15:22 +0800

    socket in server partially done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const config = require('config')
+const http = require('http')
 
 const db = require("./db");
 const AuthRouter = require("./routers/auth");
@@ -13,6 +14,12 @@ const roomProperties = require("./controllers/roomProperties")
 // create app
 const app = express()
 const apiPort = process.env.PORT || 3030
+const server = http.createServer(app)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*"
+    }
+})
 
 // add middleware
 app.use(express.urlencoded({ extended: true }))
@@ -38,6 +45,23 @@ if(config.util.getEnv('NODE_ENV') !== 'test'){
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // check port
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+server.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+io.on('connection', socket => {
+    console.log('connected!')
+    socket.on('initialize', (msg) => {
+        console.log('initialized')
+        socket.emit('initialize', msg)  
+    })
+    socket.on('chat message', (roomId, msg) => {
+        console.log('chat message sent')
+        socket.emit('chat message', msg)
+
+    })
+    socket.on('newState', (roomId, msg) => {
+        console.log('new state detected')
+        socket.emit('newState', msg)
+    })
+})
 
 module.exports = app;
