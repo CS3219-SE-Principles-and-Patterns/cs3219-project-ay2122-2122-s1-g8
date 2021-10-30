c291270e3d723cbdd4d12efd00b6c97e3d05464c
cywk <yongwankang@hotmail.com>
2021-10-30 13:01:16 +0800

    heroku

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://peerprep.herokuapp.com/api",
+  baseURL: "https://peerprep.herokuapp.com/api",
   // baseURL: "http://192.168.0.103:3030/api",
-  baseURL: "http://127.0.0.1:3030/api",
+  //baseURL: "http://127.0.0.1:3030/api",
 });
 // Account registration and login
 export const registerAccount = (payload) => api.post(`/register`, payload);
