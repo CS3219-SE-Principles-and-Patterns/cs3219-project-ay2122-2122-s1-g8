1b670f45972a04ad4d5ea39713538d69578a21a5
Oong Jie Xiang <oongjiexiang@yahoo.com>
2021-11-02 17:19:48 +0800

    room fix

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://peerprep.herokuapp.com/api",
   // baseURL: "http://192.168.0.103:3030/api",
-  //baseURL: "http://127.0.0.1:3030/api",
+  // baseURL: "http://127.0.0.1:3030/api",
 });
 // Account registration and login
 export const registerAccount = (payload) => api.post(`/register`, payload);
