2cb38606a93e862b9ff53e3dbb205b656aaf40cb
Oong Jie Xiang <oongjiexiang@yahoo.com>
2021-11-02 15:23:28 +0800

    refresh token done

--- backend/controllers/QuestionController.js
+++ backend/controllers/QuestionController.js
@@ -47,10 +47,10 @@ const question_put_update = (req, res) => {
 const question_get_details = (req, res) => {
     let zoom_id = req.params.id;
     Room.findOne({roomId: zoom_id}).then(room => {
-        console.log("Test QID")
+        // console.log("Test QID")
         let valid_questionID = room.questionID;
-        console.log(valid_questionID);
-        console.log("Test QID")
+        // console.log(valid_questionID);
+        // console.log("Test QID")
 
         Question.findById(valid_questionID)
         .then(result => {
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,13 +3,14 @@ const jwt = require('jsonwebtoken')
 function authenticateToken(req, res, next){
     //const authHeader = req.headers['authorization']
     //const token = authHeader && authHeader.split(' ')[1]
-    const authHeader = req.body['authorization'] || req.headers['authorization']
+    const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    console.log(token)
+    // console.log(token)
     if(token == null) return res.sendStatus(401)
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        //if (err) console.log(err)
-        if(err) return res.sendStatus(401)
+        if(err){
+            return res.sendStatus(401)
+        }
         req.user = user;
         next();
     })
