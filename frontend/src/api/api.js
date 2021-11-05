6f17465e705a565ab9cbbeec32fdec0eabb6fe94
caitlinjee <caitlinjeespn@gmail.com>
2021-11-05 21:32:18 +0800

    Add refresh token frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -9,12 +9,12 @@ const api = axios.create({
 export const registerAccount = (payload) => api.post(`/register`, payload);
 export const loginAccount = (payload) => api.post(`/login`, payload);
 export const updateUserStatus = (payload) => api.post(`/updateStatus`, payload);
-export const updateQuestionType = (payload) =>
-  api.post(`/updateQuestionType`, payload);
 
 // Question
 export const fetchQuestion = (payload) =>
   api.get("/question/" + payload.id, { headers: payload });
+export const updateQuestionType = (payload) =>
+  api.post(`/updateQuestionType`, payload);  
 
 // Match
 export const newMatch = (payload) => api.post("/match/new", payload);
@@ -22,6 +22,9 @@ export const matchStatus = (payload) => api.post("/match/status", payload);
 export const dropMatch = (payload) => api.post("/match/drop", payload);
 export const getRoom = (id) => api.get(`/room/${id}`);
 //export const getChat = () => api.get("/chat");
+
+// Token
+export const refreshToken = (payload) => api.post("/token", payload);
 
 const apis = {
   registerAccount,
@@ -33,6 +36,7 @@ const apis = {
   matchStatus,
   dropMatch,
   getRoom,
+  refreshToken,
   //getChat,
 };
 
